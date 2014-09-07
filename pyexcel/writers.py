2f04284bb015fa957a73046f09b654a274f5c5f5
chfw <wangc_2011@hotmail.com>
2014-09-07 16:58:58 +0100

    adding csv specific writer

--- pyexcel/writers.py
+++ pyexcel/writers.py
@@ -1,9 +1,25 @@
-class Writer:
+import csv
+
+class CSVWriter:
     def __init__(self, file):
-        self.f = open(file, "w")
+        self.f = open(file, "wb")
+        self.writer = csv.writer(self.f)
 
     def write_row(self, array):
-        self.f.write(",".join([str(x) for x in array])+"\n")
+        self.writer.writerow(array)
 
     def close(self):
         self.f.close()
+
+class Writer:
+    def __init__(self, file):
+        if file.endswith(".csv"):
+            self.writer = CSVWriter(file)
+        else:
+            raise NotImplementedError("Cannot open %s" % file)
+
+    def write_row(self, array):
+        self.writer.write_row(array)
+
+    def close(self):
+        self.writer.close()
