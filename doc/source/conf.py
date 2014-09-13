a6091ab4cdca05b0679141d71d230266378f0844
chfw <wangc_2011@hotmail.com>
2014-09-13 19:55:24 +0100

    renamed master document file

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -47,7 +47,7 @@ source_suffix = '.rst'
 #source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'pyexcel'
+master_doc = 'index'
 
 # General information about the project.
 project = u'pyexcel'
