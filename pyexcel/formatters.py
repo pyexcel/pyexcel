6bb4b6626c9d3c3f37db43991aeaa3f70c1cd505
chfw <wangc_2011@hotmail.com>
2014-09-22 22:48:53 +0100

    initial formatter

---
+++ pyexcel/formatters.py
@@ -0,0 +1,12 @@
+"""
+    pyexcel.formatters
+    ~~~~~~~~~~~~~~~~~~~
+
+    Format the data types
+
+    :copyright: (c) 2014 by C. W.
+    :license: GPL v3
+"""
+
+class ColumnFormatter:
+    pass
