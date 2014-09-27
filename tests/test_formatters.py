b8c6d854d202c291c33a648e9b5b7ab0e1d49cce
chfw <wangc_2011@hotmail.com>
2014-09-27 23:13:16 +0100

    support date and time

--- tests/test_formatters.py
+++ tests/test_formatters.py
@@ -110,13 +110,32 @@ class TestToFormatFunction:
             pyexcel.formatters.STRING_FORMAT,
             d
         )
-        assert d.isoformat() == new_d
+        assert d.strftime("%d/%m/%y") == new_d
         new_d = pyexcel.formatters.to_format(
             pyexcel.formatters.DATE_FORMAT,
             pyexcel.formatters.BOOLEAN_FORMAT,
             d
         )
         assert d == new_d
+        t = datetime.time(11,11,11)
+        new_t = pyexcel.formatters.to_format(
+            pyexcel.formatters.DATE_FORMAT,
+            pyexcel.formatters.DATE_FORMAT,
+            t
+        )
+        assert t == new_t
+        new_t = pyexcel.formatters.to_format(
+            pyexcel.formatters.DATE_FORMAT,
+            pyexcel.formatters.STRING_FORMAT,
+            t
+        )
+        assert t.strftime("%H:%M:%S") == new_t
+        new_t = pyexcel.formatters.to_format(
+            pyexcel.formatters.DATE_FORMAT,
+            pyexcel.formatters.BOOLEAN_FORMAT,
+            t
+        )
+        assert t == new_t
 
     def test_boolean_2_date(self):
         value = True
@@ -570,3 +589,38 @@ class TestSheetFormatterInXLS:
     def tearDown(self):
         if os.path.exists(self.testfile):
             os.unlink(self.testfile)
+
+
+class TestDateFormat:
+    def test_reading_date_format(self):
+        """
+        date     time
+        25/12/14 11:11:11
+        25/12/14 12:11:11
+        01/01/15 13:13:13
+        """
+        r = pyexcel.Reader(os.path.join("tests", "fixtures", "date_field.xls"))
+        assert isinstance(r[1][0], datetime.date) == True
+        assert r[1][0].strftime("%d/%m/%y") == "25/12/14"
+        assert isinstance(r[1][1], datetime.time) == True
+        assert r[1][1].strftime("%H:%M:%S") == "11:11:11"
+
+    def test_writing_date_format(self):
+        excel_filename = "testdateformat.xls"
+        data = [datetime.date(2014,12,25),
+                datetime.time(11,11,11),
+                datetime.datetime(2014,12,25,11,11,11)]
+        w = pyexcel.Writer(excel_filename)
+        w.write_row(data)
+        w.close()
+        r = pyexcel.Reader(excel_filename)
+        print r[0][0]
+        assert isinstance(r[0][0], datetime.date) == True
+        assert r[0][0].strftime("%d/%m/%y") == "25/12/14"
+        assert isinstance(r[0][1], datetime.time) == True
+        assert r[0][1].strftime("%H:%M:%S") == "11:11:11"
+        assert isinstance(r[0][2], datetime.date) == True
+        assert r[0][2].strftime("%d/%m/%y") == "25/12/14"
+        os.unlink(excel_filename)
+        
+        
