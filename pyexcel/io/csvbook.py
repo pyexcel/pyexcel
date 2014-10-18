f5ad6922ad1bfc64a784f3e9165dcbdb23fee64a
chfw <wangc_2011@hotmail.com>
2014-10-18 13:43:53 +0100

    suport utf-8 for csv reader

--- pyexcel/io/csvbook.py
+++ pyexcel/io/csvbook.py
@@ -1,5 +1,20 @@
 import six
 import csv
+import codecs
+
+
+class UTF8Recorder(six.Iterator):
+    """
+    Iterator that reads an encoded stream and reencodes the input to UTF-8.
+    """
+    def __init__(self, f, encoding):
+        self.reader = codecs.getreader(encoding)(f)
+        
+    def __iter__(self):
+        return self
+        
+    def __next__(self):
+        return next(self.reader).encode('utf-8')
 
 
 class CSVBook:
@@ -8,18 +23,19 @@ class CSVBook:
 
     It simply return one sheet
     """
-    def __init__(self, file, **keywords):
+    def __init__(self, file, encoding="utf-8", **keywords):
         self.array = []
         if six.PY2:
             f = open(file, 'rb')
         elif six.PY3:
             f = open(file, 'rt')
-        reader = csv.reader(f, dialect=csv.excel, **keywords)
+        utf_reader = UTF8Recorder(f, encoding)
+        reader = csv.reader(utf_reader, dialect=csv.excel, **keywords)
         longest_row_length = -1
         for row in reader:
             myrow = []
             for element in row:
-                myrow.append(element)
+                myrow.append(element.decode('utf-8'))
             if longest_row_length == -1:
                 longest_row_length = len(myrow)
             elif longest_row_length < len(myrow):
@@ -41,7 +57,7 @@ class CSVSheetWriter:
     csv file writer
 
     """
-    def __init__(self, file, name, **keywords):
+    def __init__(self, file, name, encoding="utf-8", **keywords):
         if name:
             names = file.split(".")
             file_name = "%s_%s.%s" % (names[0], name, names[1])
@@ -52,6 +68,7 @@ class CSVSheetWriter:
             self.f = open(file_name, "wb")
         elif six.PY3:
             self.f = open(file_name, "w", newline="")
+        self.encoding = encoding
         self.writer = csv.writer(self.f, **keywords)
 
     def set_size(self, size):
@@ -61,7 +78,7 @@ class CSVSheetWriter:
         """
         write a row into the file
         """
-        self.writer.writerow(array)
+        self.writer.writerow([six.text_type(s if s != None else '').encode(self.encoding) for s in array])
 
     def close(self):
         """
