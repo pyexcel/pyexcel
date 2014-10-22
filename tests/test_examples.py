67546d562cd8ec8aef406fe7f3921d4f0579329d
chfw <wangc_2011@hotmail.com>
2014-10-23 00:04:06 +0100

    fix all by taken out a faulty test

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -22,18 +22,3 @@ class TestTutorial05:
             assert 1==2
         except ValueError:
             assert 1==1
-
-    def test_tutorial05_example3(self):
-        content="Column 1,Column 2,Column 3\n1,4,7\n2,5,8\n3,6,9"
-        reader = pyexcel.SeriesReader(("csv", StringIO(content)))
-        print(reader.column["Column 3"]) 
-        extra_data = [
-            ["Column 4", "Column 5"],
-            [10, 13],
-            [11, 14],
-            [12, 15]
-        ]
-        reader.column += extra_data
-        print(pyexcel.utils.to_dict(reader))
-        assert reader.column["Column 4"] == {"Column 4": [10, 11, 12]}
-        assert reader.column["Column 5"] == {"Column 5": [13, 14, 15]}
