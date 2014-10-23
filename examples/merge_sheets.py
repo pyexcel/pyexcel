37e1d77bfb0274f20d666ce43739cc54bdb55f7d
chfw <wangc_2011@hotmail.com>
2014-10-23 20:31:30 +0100

    grouping

--- examples/merge_sheets.py
+++
@@ -1,8 +0,0 @@
-import pyexcel
-import glob
-merged = pyexcel.Reader()
-for file in glob.glob("*.ods"):
-    merged += pyexcel.Reader(file)
-writer = pyexcel.Writer("merged.csv")
-writer.write_reader(merged)
-writer.close()
