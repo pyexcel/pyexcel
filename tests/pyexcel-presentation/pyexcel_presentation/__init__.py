d1b48bc8b79fec7021bdce01ca4fefac2128fb14
chfw <wangc_2011@hotmail.com>
2014-11-27 09:26:08 +0000

    bug fix

--- tests/pyexcel-presentation/pyexcel_presentation/__init__.py
+++ tests/pyexcel-presentation/pyexcel_presentation/__init__.py
@@ -1,14 +1,10 @@
 from pyexcel.presentation import STRINGIFICATION
-import sys
 
 def present_matrix(matrix_instance):
     return "pyexcel.sheets.matrix.Matrix"
 
 def class_name(name):
-    if sys.version_info[0] > 2:
-        return "<class '%s'>" % name
-    else:
-        return name
+    return "<class '%s'>" % name
 
 STRINGIFICATION[class_name("pyexcel.sheets.matrix.Matrix")] = present_matrix
 
