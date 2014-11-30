4edfa3b057dfc2ba9e503528282c928a23a2e4c8
chfw <wangc_2011@hotmail.com>
2014-11-30 10:39:13 +0000

    adjust the long tables: credit: https://github.com/snide/sphinx_rtd_theme/issues/117

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -108,8 +108,17 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+os_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if os_rtd:
+    html_theme = 'default'
+else:
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
+html_context = {
+    'css_files': ['_static/theme_overrides.css']
+}
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
