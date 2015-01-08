b409f3fb4eda8b827d814f4a9e76229e5a87e12c
chfw <wangc_2011@hotmail.com>
2015-01-08 20:10:13 +0000

    fix typo in imports

--- pyexcel/__init__.py
+++ pyexcel/__init__.py
@@ -53,6 +53,6 @@ from .cookbook import (
     merge_all_to_a_book,
     split_a_book,
     extract_a_sheet_from_a_book)
-from excel import ExcelInput, FileIO
+from .excel import ExcelInput, FileIO
 
 __VERSION__ = '0.1.2'
