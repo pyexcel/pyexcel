5cda4ab947287591729b50f502d6e62238632726
chfw <wangc_2011@hotmail.com>
2015-08-04 22:00:15 +0100

    skip import xls into database example

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -5,14 +5,17 @@ import glob2
 import os
 
 
+BLACK_LIST = ['pyexcel_server', 'import_xls_into_database']
+
 class TestAllExamples:
     def test_them(self):
         base = os.getcwd()
         example_files = glob2.glob(os.path.join('examples', '**', '*.py'))
         file_registry = {}
         for abs_file_path in example_files:
-            if 'pyexcel_server' in abs_file_path:
-                continue
+            for item in BLACK_LIST:
+                if item in abs_file_path:
+                    continue
             path, file_name = os.path.split(abs_file_path)
             if path not in file_registry:
                 file_registry[path] = [file_name]
