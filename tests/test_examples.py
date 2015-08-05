2e5640ccc6cedaaf48523771878bb3d6fd7d6724
chfw <wangc_2011@hotmail.com>
2015-08-05 21:13:36 +0100

    bug fix

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -7,16 +7,9 @@ def load_from_file(mod_name, file_ext):
     func_inst = None
     py_mod = None
     expected_main = 'main'
-
-    #if file_ext.lower() == '.py':
     py_mod = imp.load_source(mod_name, os.path.join(mod_name, file_ext))
-
-    #elif file_ext.lower() == '.pyc':
-    #    py_mod = imp.load_compiled(mod_name, os.join(mod_name, file_ext))
-
     if py_mod is not None and hasattr(py_mod, expected_main):
         func_inst = getattr(py_mod, expected_main)
-
     return func_inst
 
 
@@ -36,11 +29,8 @@ class TestAllExamples:
                 file_registry[path].append(file_name)
 
         for path in file_registry:
-            #test_directory = os.path.join(base, path)
-            #os.chdir(test_directory)
             for file_name in file_registry[path]:
-                print "###################"+os.path.join(path, file_name)
+                print("testing "+os.path.join(path, file_name))
                 fn = load_from_file(path, file_name)
                 if fn is not None:
                     fn(path)
-        #os.chdir(base)
