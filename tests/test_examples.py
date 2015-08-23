554877b5a32749171a3b293eed194d26490877a8
chfw <wangc_2011@hotmail.com>
2015-08-23 01:12:37 +0100

    tets pyexcel_server.py

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,12 +1,13 @@
 import glob2
 import os
 import imp
+import json
+import pyexcel as pe
 
 
-def load_from_file(mod_name, file_ext):
+def load_from_file(mod_name, file_ext, expected_main='main'):
     func_inst = None
     py_mod = None
-    expected_main = 'main'
     py_mod = imp.load_source(mod_name, os.path.join(mod_name, file_ext))
     if py_mod is not None and hasattr(py_mod, expected_main):
         func_inst = getattr(py_mod, expected_main)
@@ -34,3 +35,49 @@ class TestAllExamples:
                 fn = load_from_file(path, file_name)
                 if fn is not None:
                     fn(path)
+
+class TestPyexcelServer:
+    def setUp(self):
+        app = load_from_file(os.path.join('examples', 'memoryfile'), 'pyexcel_server.py', 'app')
+        self.app = app.test_client()
+
+    def test_upload(self):
+        data = [
+            ['X', 'Y', 'Z'],
+            [1, 2, 3],
+            [4, 5, 6]
+        ]
+        expected = {
+            "result": {
+                "X": [
+                    "1",
+                    "4"
+                ],
+                "Y": [
+                    "2",
+                    "5"
+                ],
+                "Z": [
+                    "3",
+                    "6"
+                ]
+            }
+        }
+        io = pe.save_as(dest_file_type='csv', array=data)
+        response = self.app.post('/upload', buffered=True,
+                                 data = {"excel": (io, "test.csv")},
+                                 content_type="multipart/form-data")
+        assert json.loads(response.data) == expected
+
+    def test_download(self):
+        response = self.app.get('/download')
+        ret = pe.get_array(file_type="csv", file_content=response.data)
+        assert ret == [
+            ["REVIEW_DATE","AUTHOR","ISBN","DISCOUNTED_PRICE"],
+            ["1985/01/21","Douglas Adams",'0345391802','5.95'],
+            ["1990/01/12","Douglas Hofstadter",'0465026567','9.95'],
+            ["1998/07/15","Timothy \"The Parser\" Campbell",'0968411304','18.99'],
+            ["1999/12/03","Richard Friedman",'0060630353','5.95'],
+            ["2004/10/04","Randel Helms",'0879755725','4.5']
+        ]
+
