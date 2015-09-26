350b6fbf13b9ddaa4d58fd0b65e8028774573013
chfw <wangc_2011@hotmail.com>
2015-09-26 23:30:36 +0100

    make jsonify.py testable

--- examples/basics/jsonify.py
+++ examples/basics/jsonify.py
@@ -1,6 +1,17 @@
+"""
+jsonify.py
+:copyright: (c) 2014-2015 by Onni Software Ltd.
+:license: New BSD License, see LICENSE for more details
+"""
+import os
 import pyexcel as pe
 import json
 
-# "example.xls","example.xlsx","example.ods", "example.xlsm"
-sheet = pe.Sheet("example.csv")
-print(json.dumps(sheet.to_array()))
+def main(base_dir):
+    # "example.xls","example.xlsx","example.ods", "example.xlsm"
+    sheet = pe.Sheet("example.csv")
+    print(json.dumps(sheet.to_array()))
+
+
+if __name__ == '__main__':
+    main(os.getcwd())
