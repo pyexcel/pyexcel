63f6cf5312c6180863e3ace607bb4aff45ee4e8b
chfw <wangc_2011@hotmail.com>
2016-01-11 22:23:20 +0000

    test sheet stream

---
+++ tests/test_sheet_stream.py
@@ -0,0 +1,18 @@
+from pyexcel.sources import SheetSource
+from pyexcel.sheets import SheetStream
+from pyexcel_io import get_io
+from textwrap import dedent
+
+def test_save_to():
+    file_type = 'csv'
+    io = get_io(file_type)
+    g = (i for i in [[1,2],[3,4]])
+    ss = SheetSource((file_type, io), lineterminator='\n')
+    sheet_stream = SheetStream("test", g)
+    sheet_stream.save_to(ss)
+    content = io.getvalue()
+    expected = dedent("""\
+    1,2
+    3,4
+    """)
+    assert content == expected
