38926e806a9ff9fd5986c031a92f38be30b50146
chfw <wangc_2011@hotmail.com>
2016-03-12 22:47:19 +0000

    minor correction

--- document.sh
+++ document.sh
@@ -1,6 +1,6 @@
 python setup.py install
 rm doc/source/generated/*.rst
-del doc/source/iapi/*.rst
+rm doc/source/iapi/*.rst
 sphinx-autogen -o doc/source/generated doc/source/api.rst
 sphinx-autogen -o doc/source/iapi doc/source/iapi.rst
 sphinx-build -b html doc/source doc/build
