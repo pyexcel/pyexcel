48c5b2279d2699fe033a0ff3df732981d00e2a0d
chfw <wangc_2011@hotmail.com>
2016-04-13 17:25:18 +0100

    accommendate external file stream for output

--- tests/test_sheet_stream.py
+++ tests/test_sheet_stream.py
@@ -1,12 +1,12 @@
 from pyexcel.sources.file import SheetSource
 from pyexcel.sheets import SheetStream
-from pyexcel_io import get_io
+from pyexcel_io.manager import RWManager
 from textwrap import dedent
 
 
 def test_save_to():
     file_type = 'csv'
-    io = get_io(file_type)
+    io = RWManager.get_io(file_type)
     g = (i for i in [[1,2],[3,4]])
     ss = SheetSource((file_type, io), lineterminator='\n')
     sheet_stream = SheetStream("test", g)
