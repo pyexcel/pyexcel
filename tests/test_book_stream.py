735bb01606b5f4389f203415dbe99f6e20abd2e9
chfw <wangc_2011@hotmail.com>
2016-04-15 21:47:33 +0100

    unit test for bookstream

---
+++ tests/test_book_stream.py
@@ -0,0 +1,22 @@
+from pyexcel.book import BookStream
+
+
+def test_book_stream():
+    bs = BookStream()
+    assert bs.number_of_sheets() == 0
+
+
+def test_load_from_empty_sheets():
+    bs = BookStream()
+    bs.load_from_sheets(None)
+    assert bs.number_of_sheets() == 0
+
+
+def test_key_sorting():
+    adict = {
+        "cd": [[1,3]],
+        "ab": [[2,3]]
+    }
+    bs = BookStream(adict)
+    # bs[0] should be 'ab' : SheetStream([[2,3]])
+    assert bs[0].payload == [[2,3]]
