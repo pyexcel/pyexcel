2cd3a519adee3b65f23c111c52871fb29827b680
chfw <wangc_2011@hotmail.com>
2016-04-28 08:59:18 +0100

    update travis for rnd requirement which is temporary and fix pypy3 problem

--- setup.py
+++ setup.py
@@ -5,42 +5,17 @@ except ImportError:
     use_setuptools()
     from setuptools import setup, find_packages
 
-with open("README.rst", 'r') as readme:
-    README_txt = readme.read()
-
-with open("CHANGELOG.rst", 'r') as changelog:
-    README_txt += changelog.read()
-
-dependencies = [
-    'pyexcel-io>=0.1.0',
-    'texttable>=0.8.2',
-]
-
-extras = {
-    'xls': ['pyexcel-xls>=0.1.0'],
-    'xlsx': ['pyexcel-xlsx>=0.1.0'],
-    'ods': ['pyexcel-ods3>=0.1.0'],
-}
-
-
-setup(
-    name='pyexcel',
-    author='C.W.',
-    version='0.2.2',
-    author_email='wangc_2011 (at) hotmail.com',
-    url='https://github.com/pyexcel/pyexcel',
-    description='A wrapper library that provides one API to read, manipulate and write data in different excel formats',
-    install_requires=dependencies,
-    extras_require=extras,
-    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-    include_package_data=True,
-    long_description=README_txt,
-    zip_safe=False,
-    tests_require=['nose'],
-    keywords=[
-        'excel',
-        'python',
-        'pyexcel',
+NAME = 'pyexcel'
+AUTHOR = 'C.W.'
+VERSION = '0.2.2'
+EMAIL = 'wangc_2011 (at) hotmail.com'
+LICENSE = 'New BSD'
+PACKAGES = find_packages(exclude=['ez_setup', 'examples', 'tests'])
+DESCRIPTION = 'A wrapper library that provides one API to read, manipulate and write data in different excel formats'
+KEYWORDS = [
+   'excel',
+   'python',
+   'pyexcel',
         'tsv',
         'tsvz'
         'csv',
@@ -48,15 +23,26 @@ setup(
         'xls',
         'xlsx',
         'ods'
-    ],
-    license='New BSD',
-    classifiers=[
-        'Topic :: Office/Business',
-        'Topic :: Utilities',
-        'Topic :: Software Development :: Libraries',
-        'Programming Language :: Python',
-        'License :: OSI Approved :: BSD License',
-        'Intended Audience :: Developers',
+]
+
+INSTALL_REQUIRES = [
+    'pyexcel-io>=0.1.0',
+    'texttable>=0.8.2',
+]
+
+EXTRAS_REQUIRE = {
+    'xls': ['pyexcel-xls>=0.1.0'],
+    'xlsx': ['pyexcel-xlsx>=0.1.0'],
+    'ods': ['pyexcel-ods3>=0.1.0'],
+}
+
+CLASSIFIERS = [
+    'Topic :: Office/Business',
+    'Topic :: Utilities',
+    'Topic :: Software Development :: Libraries',
+    'Programming Language :: Python',
+    'License :: OSI Approved :: BSD License',
+    'Intended Audience :: Developers',
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
@@ -66,5 +52,38 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: PyPy'
-    ]
-)
+]
+
+
+def read_files(*files):
+    """Read files into setup"""
+    text = ""
+    for single_file in files:
+        text = text + read(single_file) + "\n"
+    return text
+
+
+def read(afile):
+    """Read a file into setup"""
+    with open(afile, 'r') as opened_file:
+        return opened_file.read()
+
+
+if __name__ == '__main__':
+    setup(
+        name=NAME,
+        author=AUTHOR,
+        version=VERSION,
+        author_email=EMAIL,
+        description=DESCRIPTION,
+        install_requires=INSTALL_REQUIRES,
+        keywords=KEYWORDS,
+        extras_require=EXTRAS_REQUIRE,
+        packages=PACKAGES,
+        include_package_data=True,
+        long_description=read_files('README.rst', 'CHANGELOG.rst'),
+        zip_safe=False,
+        tests_require=['nose'],
+        license=LICENSE,
+        classifiers=CLASSIFIERS
+    )
