d40d904e8c46c99c7c33eccc13405548c31e3abb
chfw <wangc_2011@hotmail.com>
2016-05-17 22:51:41 +0100

    reorganize code locations for cohension

--- pyexcel/sources/__init__.py
+++ pyexcel/sources/__init__.py
@@ -1,3 +1,5 @@
-from . import file_source_output, database, read
+from . import file_source_output, database
+from . import file_source_input, http, pydata
 
-sources = file_source_output.sources + database.sources + read.sources
+sources = (http.sources + file_source_input.sources + pydata.sources +
+           file_source_output.sources + database.sources)
--- pyexcel/sources/read/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import input_source, http, pydata
-
-sources = http.sources + input_source.sources + pydata.sources
