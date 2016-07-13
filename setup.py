3476bbed84f81df513b7ecb6c323205a2a0790c3
chfw <wangc_2011@hotmail.com>
2016-07-13 17:58:19 +0100

    prepare for a hot fix release, 0.2.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 NAME = 'pyexcel'
 AUTHOR = 'C.W.'
-VERSION = '0.2.3'
+VERSION = '0.2.4'
 EMAIL = 'wangc_2011 (at) hotmail.com'
 LICENSE = 'New BSD'
 PACKAGES = find_packages(exclude=['ez_setup', 'examples', 'tests'])
@@ -30,7 +30,7 @@ KEYWORDS = [
 ]
 
 INSTALL_REQUIRES = [
-    'pyexcel-io>=0.2.0',
+    'pyexcel-io>=0.2.1',
 ]
 
 EXTRAS_REQUIRE = {
