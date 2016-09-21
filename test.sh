0a957d43216e0ba4def614f24afd4ef682c83e4b
chfw <wangc_2011@hotmail.com>
2016-09-21 22:26:54 +0100

    more output to see why pypy fails

--- test.sh
+++ test.sh
@@ -6,5 +6,5 @@ cd ../../
 
 
 pip freeze
-nosetests --with-cov --cover-package pyexcel --cover-package tests --with-doctest --doctest-extension=.rst tests README.rst pyexcel && flake8 . --exclude=.moban.d --builtins=unicode,xrange,long
+nosetests --with-cov --cover-package pyexcel --cover-package tests --with-doctest --doctest-extension=.rst tests README.rst pyexcel -s && flake8 . --exclude=.moban.d --builtins=unicode,xrange,long
 
