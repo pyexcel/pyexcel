69a04ad630e94765eb1479724205dc7b77d74226
chfw <wangc_2011@hotmail.com>
2017-03-14 22:48:13 +0000

    move ugly code into internal folder

---
+++ pyexcel/internal/__init__.py
@@ -0,0 +1,65 @@
+import logging
+
+from pkgutil import iter_modules
+from collections import defaultdict
+
+log = logging.getLogger(__name__)
+
+soft_renderer_registry = defaultdict(list)
+soft_parser_registry = defaultdict(list)
+
+UPGRADE_MESSAGE = "Please upgrade the plugin '%s' according to \
+plugin compactibility table."
+
+
+class UpgradePlugin(Exception):
+    pass
+
+
+def pre_register(registry, library_meta, module_name):
+    if not isinstance(library_meta, dict):
+        plugin = module_name.replace('_', '-')
+        raise UpgradePlugin(UPGRADE_MESSAGE % plugin)
+    library_import_path = "%s.%s" % (module_name, library_meta['submodule'])
+    for file_type in library_meta['file_types']:
+        registry[file_type].append(
+            (library_import_path, library_meta['submodule']))
+    log.debug("pre-register :" + ','.join(library_meta['file_types']))
+
+
+def dynamic_load_library(library_import_path):
+    __import__(library_import_path[0])
+
+
+def preload_a_renderer(registry, file_type):
+    __file_type = file_type.lower()
+    if __file_type in registry:
+        debug_path = []
+        for path in registry[__file_type]:
+            dynamic_load_library(path)
+            debug_path.append(path)
+        log.debug("preload :" + __file_type + ":" + ','.join(path))
+        # once loaded, forgot it
+        registry.pop(__file_type)
+
+
+black_list = ['pyexcel_io', 'pyexcel_webio',
+              'pyexcel_xlsx', 'pyexcel_xls',
+              'pyexcel_ods3', 'pyexcel_ods',
+              'pyexcel_odsr', 'pyexcel_xlsxw']
+
+
+for _, module_name, ispkg in iter_modules():
+    if module_name in black_list:
+        continue
+
+    if ispkg and module_name.startswith('pyexcel_'):
+        try:
+            plugin = __import__(module_name)
+            if hasattr(plugin, '__pyexcel_renderer_plugins__'):
+                for meta in plugin.__pyexcel_renderer_plugins__:
+                    pre_register(soft_renderer_registry, meta, module_name)
+        except Exception as e:
+            log.info("Failed to import %s due to %s" % (module_name, str(e)),
+                     exc_info=True)
+            continue
--- pyexcel/renderers/__init__.py
+++ pyexcel/renderers/__init__.py
@@ -1,42 +1,13 @@
-# flake8: noqa
-import logging
-
-
-from .factory import renderer_registry, soft_renderer_registry, pre_register, preload_a_renderer
-from . import _texttable, _excel, _database
-from pkgutil import iter_modules
-
-log = logging.getLogger(__name__)
-
-
-black_list = ['pyexcel_io', 'pyexcel_webio',
-              'pyexcel_xlsx', 'pyexcel_xls',
-              'pyexcel_ods3', 'pyexcel_ods',
-              'pyexcel_odsr', 'pyexcel_xlsxw']
-
-
-for _, module_name, ispkg in iter_modules():
-    if module_name in black_list:
-        continue
-
-    if ispkg and module_name.startswith('pyexcel_'):
-        try:
-            plugin = __import__(module_name)
-            if hasattr(plugin, '__pyexcel_renderer_plugins__'):
-                for meta in plugin.__pyexcel_renderer_plugins__:
-                    #__import__("%s.%s" % (module_name, p))
-                    pre_register(meta, module_name)
-        except Exception as e:
-            log.info("Failed to import %s due to %s" % (module_name, str(e)),
-                     exc_info=True)
-            continue
+from .factory import renderer_registry
+from pyexcel.internal import soft_renderer_registry, preload_a_renderer
+from . import _texttable, _excel, _database  # noqa
 
 
 def get_renderer(file_type):
     __file_type = None
     if file_type:
         __file_type = file_type.lower()
-    preload_a_renderer(file_type)
+    preload_a_renderer(soft_renderer_registry, file_type)
     renderer_class = renderer_registry.get(__file_type)
     if renderer_class is None:
         raise Exception("No renderer found for %s" % file_type)
