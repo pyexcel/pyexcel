110353aece178c502ed410099fc2f61ae6c1803d
chfw <wangc_2011@hotmail.com>
2017-03-14 22:23:28 +0000

    dynamically load external renderers

--- pyexcel/renderers/__init__.py
+++ pyexcel/renderers/__init__.py
@@ -2,7 +2,7 @@
 import logging
 
 
-from .factory import renderer_registry
+from .factory import renderer_registry, soft_renderer_registry, pre_register, preload_a_renderer
 from . import _texttable, _excel, _database
 from pkgutil import iter_modules
 
@@ -14,6 +14,7 @@ black_list = ['pyexcel_io', 'pyexcel_webio',
               'pyexcel_ods3', 'pyexcel_ods',
               'pyexcel_odsr', 'pyexcel_xlsxw']
 
+
 for _, module_name, ispkg in iter_modules():
     if module_name in black_list:
         continue
@@ -22,8 +23,9 @@ for _, module_name, ispkg in iter_modules():
         try:
             plugin = __import__(module_name)
             if hasattr(plugin, '__pyexcel_renderer_plugins__'):
-                for p in plugin.__pyexcel_renderer_plugins__:
-                    __import__("%s.%s" % (module_name, p))
+                for meta in plugin.__pyexcel_renderer_plugins__:
+                    #__import__("%s.%s" % (module_name, p))
+                    pre_register(meta, module_name)
         except Exception as e:
             log.info("Failed to import %s due to %s" % (module_name, str(e)),
                      exc_info=True)
@@ -34,6 +36,7 @@ def get_renderer(file_type):
     __file_type = None
     if file_type:
         __file_type = file_type.lower()
+    preload_a_renderer(file_type)
     renderer_class = renderer_registry.get(__file_type)
     if renderer_class is None:
         raise Exception("No renderer found for %s" % file_type)
@@ -41,4 +44,4 @@ def get_renderer(file_type):
 
 
 def get_all_file_types():
-    return renderer_registry.keys()
+    return renderer_registry.keys() + soft_renderer_registry.keys()
--- pyexcel/renderers/factory.py
+++ pyexcel/renderers/factory.py
@@ -1,10 +1,16 @@
 import logging
 
 from pyexcel._compact import StringIO, with_metaclass
+from collections import defaultdict
+
+
+UPGRADE_MESSAGE = "Please upgrade the plugin '%s' according to \
+plugin compactibility table."
 
 
 log = logging.getLogger(__name__)
 renderer_registry = {}
+soft_renderer_registry = defaultdict(list)
 
 
 def _register_renderer(renderer_cls):
@@ -13,6 +19,37 @@ def _register_renderer(renderer_cls):
         __file_types.append(file_type)
         renderer_registry[file_type] = renderer_cls
     log.debug("%s: %s" % (",".join(__file_types), renderer_cls))
+
+
+class UpgradePlugin(Exception):
+    pass
+
+
+def pre_register(library_meta, module_name):
+    if not isinstance(library_meta, dict):
+        plugin = module_name.replace('_', '-')
+        raise UpgradePlugin(UPGRADE_MESSAGE % plugin)
+    library_import_path = "%s.%s" % (module_name, library_meta['submodule'])
+    for file_type in library_meta['file_types']:
+        soft_renderer_registry[file_type].append(
+            (library_import_path, library_meta['submodule']))
+    log.debug("pre-register :" + ','.join(library_meta['file_types']))
+
+
+def dynamic_load_library(library_import_path):
+    __import__(library_import_path[0])
+
+
+def preload_a_renderer(file_type):
+    __file_type = file_type.lower()
+    if __file_type in soft_renderer_registry:
+        debug_path = []
+        for path in soft_renderer_registry[__file_type]:
+            dynamic_load_library(path)
+            debug_path.append(path)
+        log.debug("preload :" + __file_type + ":" + ','.join(path))
+        # once loaded, forgot it
+        soft_renderer_registry.pop(__file_type)
 
 
 class MetaForRendererRegistryOnly(type):
