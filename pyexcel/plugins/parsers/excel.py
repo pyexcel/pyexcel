1d7b4a349f80b9d5afd299cae3819b6c4479313a
chfw <wangc_2011@hotmail.com>
2017-06-07 22:01:06 +0100

    code refactoring on excel renderer

--- pyexcel/plugins/parsers/excel.py
+++ pyexcel/plugins/parsers/excel.py
@@ -13,30 +13,24 @@ from pyexcel_io import get_data, iget_data
 
 class ExcelParser(AbstractParser):
     """get data from excel files"""
-    def parse_file(self, file_name, on_demand=False, **keywords):
-        if on_demand:
-            sheets, reader = iget_data(file_name, **keywords)
-            self._free_me_up_later(reader)
-        else:
-            sheets = get_data(file_name, **keywords)
-        return sheets
+    def parse_file(self, file_name, **keywords):
+        return self._parse_any(file_name, **keywords)
 
-    def parse_file_stream(self, file_stream, on_demand=False, **keywords):
-        if on_demand:
-            sheets, reader = iget_data(file_stream, file_type=self._file_type,
-                                       **keywords)
-            self._free_me_up_later(reader)
-        else:
-            sheets = get_data(
-                file_stream, file_type=self._file_type, **keywords)
-        return sheets
+    def parse_file_stream(self, file_stream, **keywords):
+        return self._parse_any(
+            file_stream, file_type=self._file_type, **keywords)
 
-    def parse_file_content(self, file_content, on_demand=False, **keywords):
+    def parse_file_content(self, file_content, **keywords):
+        return self._parse_any(
+            file_content, file_type=self._file_type, **keywords)
+
+    def _parse_any(self, anything, on_demand=False,
+                   file_type=None, **keywords):
         if on_demand:
             sheets, reader = iget_data(
-                file_content, file_type=self._file_type, **keywords)
+                anything, file_type=file_type, **keywords)
             self._free_me_up_later(reader)
         else:
             sheets = get_data(
-                file_content, file_type=self._file_type, **keywords)
+                anything, file_type=file_type, **keywords)
         return sheets
