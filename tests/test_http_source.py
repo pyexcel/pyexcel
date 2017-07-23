d0bd2133e1e4a707ca9e26bd788a6c961fa2bb67
chfw <wangc_2011@hotmail.com>
2017-07-23 17:51:13 +0100

    fix broken tests

--- tests/test_http_source.py
+++ tests/test_http_source.py
@@ -2,17 +2,36 @@ from unittest import TestCase
 from mock import patch, MagicMock
 import pyexcel as pe
 from textwrap import dedent
+from pyexcel._compact import PY2
 
 
 class TestHttpBookSource(TestCase):
-    @patch('pyexcel._compact.request.urlopen')
-    def test_url_source_via_content_type(self, mock_open):
+    def setUp(self):
+        self.patcher = patch('pyexcel._compact.request.urlopen')
+        mock_open = self.patcher.start()
         m = MagicMock()
-        x = MagicMock()
-        x.type.return_value = "text/csv"
-        m.info.return_value = x
+        self.mocked_info = MagicMock()
+        m.info.return_value = self.mocked_info
         m.read.return_value = "1,2,3"
         mock_open.return_value = m
+
+    def tearDown(self):
+        self.patcher.stop()
+
+    def test_url_source_via_content_type(self):
+        book = pe.get_book(url="xx.csv")
+        if PY2:
+            self.mocked_info.type.return_value = "text/csv"
+        else:
+            self.mocked_info.get_content_type.return_value = "text/csv"
+        content = dedent("""
+        csv:
+        +---+---+---+
+        | 1 | 2 | 3 |
+        +---+---+---+""").strip('\n')
+        self.assertEqual(str(book), content)
+
+    def test_url_source_via_file_suffix(self):
         book = pe.get_book(url="xx.csv")
         content = dedent("""
         csv:
@@ -21,30 +40,7 @@ class TestHttpBookSource(TestCase):
         +---+---+---+""").strip('\n')
         self.assertEqual(str(book), content)
 
-    @patch('pyexcel._compact.request.urlopen')
-    def test_url_source_via_file_suffix(self, mock_open):
-        m = MagicMock()
-        x = MagicMock()
-        x.type.return_value = "text"
-        m.info.return_value = x
-        m.read.return_value = "1,2,3"
-        mock_open.return_value = m
-        book = pe.get_book(url="xx.csv")
-        content = dedent("""
-        csv:
-        +---+---+---+
-        | 1 | 2 | 3 |
-        +---+---+---+""").strip('\n')
-        self.assertEqual(str(book), content)
-
-    @patch('pyexcel._compact.request.urlopen')
-    def test_url_source_via_file_suffix_get_sheet(self, mock_open):
-        m = MagicMock()
-        x = MagicMock()
-        x.type.return_value = "text"
-        m.info.return_value = x
-        m.read.return_value = "1,2,3"
-        mock_open.return_value = m
+    def test_url_source_via_file_suffix_get_sheet(self):
         sheet = pe.get_sheet(url="xx.csv")
         content = dedent("""
         csv:
