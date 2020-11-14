f7698fd95ede5feb56ef94b34055a5490272bbfc
chfw <wangc_2011@hotmail.com>
2020-11-14 23:11:14 +0000

    :microscope: more test cases for my assurance

--- tests/test_sheet_access.py
+++ tests/test_sheet_access.py
@@ -17,6 +17,22 @@ class TestSheetAccess(PyexcelSheetBase):
 
         self.assertEqual(value, self.sheet[cell])
 
+    def test_out_of_row_bound_write(self):
+        value = self.get_random_char()
+        column = self.get_random_char() + self.get_random_char()
+        cell = column + str(random.randint(9, 30))
+        self.sheet[cell] = value
+
+        self.assertEqual(value, self.sheet[cell])
+
+    def test_out_of_column_bound_write(self):
+        value = self.get_random_char()
+        column = self.get_random_char() + self.get_random_char()
+        cell = 'A' + column + str(random.randint(9, 30))
+        self.sheet[cell] = value
+
+        self.assertEqual(value, self.sheet[cell])
+
     def test_out_of_bounds_read(self):
         with self.assertRaises(IndexError):
             self.sheet[0, 20]
