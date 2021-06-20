ea694533623647fb572bb9a50d95357e54066f53
jaska <wangc_2011@hotmail.com>
2021-06-20 08:57:04 +0100

    Update test_bug_fixes.py

--- tests/test_bug_fixes.py
+++ tests/test_bug_fixes.py
@@ -547,6 +547,7 @@ def test_issue_241():
         glob.glob("tests/fixtures/issue_241/*.csv"), "issue_241.xlsx"
     )
     book = get_book(file_name="issue_241.xlsx")
+    book.sort_sheet_names()
     expected = dedent(
         """
     1.csv:
