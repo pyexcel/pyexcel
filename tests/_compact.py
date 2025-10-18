45745c75db0a65707e707cafe28812f6294b8328
chfw <wangc_2011@hotmail.com>
2025-10-18 15:23:47 +0100

    :bug: fix minor bug in doc

--- tests/_compact.py
+++ tests/_compact.py
@@ -2,10 +2,7 @@
 import sys
 
 PY2 = sys.version_info[0] == 2
-if sys.version_info[0] == 2 and sys.version_info[1] < 7:
-    from ordereddict import OrderedDict
-else:
-    from collections import OrderedDict
+from collections import OrderedDict
 
 if PY2:
     from StringIO import StringIO
