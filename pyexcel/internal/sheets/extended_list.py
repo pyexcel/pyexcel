7520efe639fb2725f8612688c50ab8367cd2f6e3
chfw <wangc_2011@hotmail.com>
2025-10-23 19:42:05 +0100

    :lipstick: code refactoring fix

--- pyexcel/internal/sheets/extended_list.py
+++ pyexcel/internal/sheets/extended_list.py
@@ -1,9 +1,10 @@
 from collections import Counter
-from pyexcel import get_sheet
 
 
 class PyexcelList(list):
     def value_counts(self):
+        from pyexcel import get_sheet
+
         c = Counter(self)
         sheet = get_sheet(adict=c)
         sheet.rownames = ["names", "counts"]
