6feda24e7e8cb0775b34b03d7a34a0901a346f07
chfw <chfw@users.noreply.github.com>
2025-10-25 09:26:26 +0000

    This is an auto-commit, updating project meta data, such as changelog.rst, contributors.rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ author = 'C.W.'
 # The short X.Y version
 version = '0.7.4'
 # The full version, including alpha/beta/rc tags
-release = '0.7.3'
+release = '0.7.4'
 
 # -- General configuration ---------------------------------------------------
 
