3734c78fc58e4cc180038ed09437dcf36f70f163
chfw <chfw@users.noreply.github.com>
2025-10-25 09:01:09 +0000

    This is an auto-commit, updating project meta data, such as changelog.rst, contributors.rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ project = 'pyexcel'
 copyright = '2014-2025 Onni Software Ltd.'
 author = 'C.W.'
 # The short X.Y version
-version = '0.7.3'
+version = '0.7.4'
 # The full version, including alpha/beta/rc tags
 release = '0.7.3'
 
--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ except (ValueError, UnicodeError, locale.Error):
 
 NAME = "pyexcel"
 AUTHOR = "C.W."
-VERSION = "0.7.3"
+VERSION = "0.7.4"
 EMAIL = "info@pyexcel.org"
 LICENSE = "New BSD"
 DESCRIPTION = (
