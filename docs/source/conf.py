dcf945eae5d2647014c69cef1aa663a37ca70310
chfw <chfw@users.noreply.github.com>
2026-06-20 15:04:22 +0000

    This is an auto-commit, updating project meta data, such as changelog.rst, contributors.rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,9 +26,9 @@ project = 'pyexcel'
 copyright = '2014-2026 Onni Software Ltd.'
 author = 'C.W.'
 # The short X.Y version
-version = '0.7.4'
+version = '0.7.5'
 # The full version, including alpha/beta/rc tags
-release = '0.7.4'
+release = '0.7.5'
 
 # -- General configuration ---------------------------------------------------
 
